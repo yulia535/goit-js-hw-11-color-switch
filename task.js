1b921d482b5985c4d36e4cd51826d8b0263d3618
Yulia Sukhar <yulia-s@i.ua>
2021-03-04 19:05:18 +0200

    11/1

---
+++ task.js
@@ -0,0 +1,36 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+const body = document.querySelector('body');
+const colorIndex = colors.length - 1;
+
+let changeColor;
+
+const addAt = function () {
+  body.setAttribute(
+    'bgcolor',
+    colors[randomIntegerFromInterval(0, colorIndex)]
+  );
+};
+
+startBtn.addEventListener('click', () => {
+  changeColor = setInterval(addAt, 1000);
+  startBtn.setAttribute('disabled', 'true');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(changeColor, 1000);
+  startBtn.removeAttribute('disabled');
+});
